e2fe57d1936d5f6fca05958d9d133c860eb696a7
Bayupratama28 <bayupratama2805@gmail.com>
2019-04-13 14:16:38 +0700

    IF_ELSE.js

---
+++ IF_ELSE.js
@@ -0,0 +1,17 @@
+var nama = "Mikael" ;
+var peran = "" ;
+
+if(nama == "Mikael" && peran == ""){
+    console.log("Halo Mikael, Pilih peranmu untuk memulai game")
+}else if(nama == "Nina" && peran == "Ksatria" ){
+    console.log("Selamat datang di Dunia proxytia, Nina")
+    console.log("Halo Ksatria Nina, kamu dapat meyerang dengan senjatamu")
+}else if(nama == "Danu" && peran == "Tabib"){
+    console.log("Selamt datang di dunia Proxytia, Danu")
+    console.log("Halo Tabib Danu, kamu yang akan membantu temanmu yang terluka")
+}else if(nama == "Zero" && peran == "Penyihir"){
+  console.log("Selamat datang di Dunia Proxytia, Zero")
+  console.log("Halo Penyihir Zero, ciptakan keajaiban yang menciptakan kemenangan!")
+}else{
+  console.log("Nama Harus di Isi")
+}  
